cb4a6aa1ff93c294c0ffb8100c0e7ae4afe9089f
“itguykunal” <kunal.krishna2021@vitstudent.ac.in>
2025-08-01 02:16:54 +0530

    deploy vercel

--- src/components/sections/Testimonials.tsx
+++ src/components/sections/Testimonials.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useState, useEffect } from 'react'
-import { Button } from '../components/ui/Button'
+import { Button } from '../ui/Button'
 import Image from 'next/image'
 
 const testimonials = [
