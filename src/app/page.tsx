3be5e7ed1f7fb6d982596d3466de4bab84e405ff
“itguykunal” <kunal.krishna2021@vitstudent.ac.in>
2025-08-01 02:28:46 +0530

    deploy vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Navbar } from '../components/layout/Navbar'
-import { Hero } from '../components/sections/Hero'
-import { Features } from '../components/sections/Features'
-import { Pricing } from '../components/sections/Pricing'
-import { Testimonials } from '../components/sections/Testimonials'
-import { FAQ } from '../components/sections/FAQ'
-import { Footer } from '../components/layout/Footer'
+import Navbar from '../components/layout/Navbar'
+import Hero from '../components/sections/Hero'
+import Features from '../components/sections/Features'
+import Pricing from '../components/sections/Pricing'
+import Testimonials from '../components/sections/Testimonials'
+import FAQ from '../components/sections/FAQ'
+import Footer from '../components/layout/Footer'
 
 export default function Home() {
   return (
